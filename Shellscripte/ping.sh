4f3fc29a19d9a361690457342686a518b7c0c3cd
Philip4Life <p.mader@hotmail.com>
2014-12-04 10:29:56 +0100

    ping.sh

--- Shellscripte/ping.sh
+++ Shellscripte/ping.sh
@@ -6,15 +6,12 @@ ping -q -c4 $ID < /dev/null
 if [ $? -eq 0 ]; then
 echo " $ID Pingen zu folgendem Zeitpunkt erfolgreich: " >> /home/pi/Hausautomatisierung/logfile.file
 echo $(date +'%H-%M-%S') >> /home/pi/Hausautomatisierung/logfile.file
-echo " " >> /home/pi/Hausautomatisierung/logfile.file
-echo "17"  /sys/class/gpio/export
-sudo chmod 222 /sys/class/gpio/export /sys/class/gpio/unexport
-echo "out" > /sys/class/gpio/gpio17/direction
-echo "1" > /sys/class/gpio/gpio17/value
+python /home/pi/Hausautomatisierung/Python/led-high.py
 else 
 echo " $ID Pingen zu folgendem Zeitpunkt nicht erfolgreich: " >> /home/pi/Hausautomatisierung/logfile.file
 echo $(date +'%H-%M-%S') >> /home/pi/Hausautomatisierung/logfile.file
 echo " " >> /home/pi/Hausautomatisierung/logfile.file
+python /home/pi/Hausautomatisierung/Python/led-low.py
 fi
 
 
