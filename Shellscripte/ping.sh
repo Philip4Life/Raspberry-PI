55047436c8702de30e19793efb8f7659ea5f83ae
Philip4Life <p.mader@hotmail.com>
2015-04-15 23:56:43 +0200

    Zwischenbericht April

--- Shellscripte/ping.sh
+++ Shellscripte/ping.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
-ID=172.20.10.6
+ID=172.20.10.4
+
+#ping -q -c4 $ID < /dev/null
+#if [ $? -eq 0 ]; then
+#echo " $ID Pingen zu folgendem Zeitpunkt erfolgreich: " >> /home/pi/Hausautomatisierung/logfile.file
+#echo $(date +'%H-%M-%S') >> /home/pi/Hausautomatisierung/logfile.file
+#python /home/pi/Hausautomatisierung/Python/led-high.py
+#else 
+#echo " $ID Pingen zu folgendem Zeitpunkt nicht erfolgreich: " >> /home/pi/Hausautomatisierung/logfile.file
+#echo $(date +'%H-%M-%S') >> /home/pi/Hausautomatisierung/logfile.file
+#echo " " >> /home/pi/Hausautomatisierung/logfile.file
+##python /home/pi/Hausautomatisierung/Python/led-low.py
+#fi
+
 
 ping -q -c4 $ID < /dev/null
 if [ $? -eq 0 ]; then
-echo " $ID Pingen zu folgendem Zeitpunkt erfolgreich: " >> /home/pi/Hausautomatisierung/logfile.file
-echo $(date +'%H-%M-%S') >> /home/pi/Hausautomatisierung/logfile.file
-python /home/pi/Hausautomatisierung/Python/led-high.py
-else 
-echo " $ID Pingen zu folgendem Zeitpunkt nicht erfolgreich: " >> /home/pi/Hausautomatisierung/logfile.file
-echo $(date +'%H-%M-%S') >> /home/pi/Hausautomatisierung/logfile.file
-echo " " >> /home/pi/Hausautomatisierung/logfile.file
-python /home/pi/Hausautomatisierung/Python/led-low.py
-fi
+echo " $ID Pingen zu folgendem Zeitpunkt erfolgreich: " >> /Users/philip/Documents/Schule/PPM/PPM-Köllö/Hausautomatisierung/Sonstige Dateien/Logfiles/logfile.file
+echo $(date +'%H-%M-%S') >> /Users/philip/Documents/Schule/PPM/PPM-Köllö/Hausautomatisierung/Sonstige Dateien/Logfiles/logfile.file
+python /Users/philip/GitHub/Raspberry-PI/Python/led-high.py
+echo " " >> /Users/philip/Documents/Schule/PPM/PPM-Köllö/Hausautomatisierung/Sonstige Dateien/Logfiles/logfile.file
 
+else 
+echo " $ID Pingen zu folgendem Zeitpunkt nicht erfolgreich: " >> /Users/philip/Documents/Schule/PPM/PPM-Köllö/Hausautomatisierung/Sonstige Dateien/Logfiles/logfile.file
+echo $(date +'%H-%M-%S') >> /Users/philip/Documents/Schule/PPM/PPM-Köllö/Hausautomatisierung/Sonstige Dateien/Logfiles/logfile.file
+echo " " >> /Users/philip/Documents/Schule/PPM/PPM-Köllö/Hausautomatisierung/Sonstige Dateien/Logfiles/logfile.file
+python /Users/philip/GitHub/Raspberry-PI/Python/led-low.py
+fi
 
